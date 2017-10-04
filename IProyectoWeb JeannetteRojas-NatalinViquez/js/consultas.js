4b8563579f7fa146c12a6e021373ed33bce90ff9
Jeannette Rojas <janerrodriguez92@gmail.com>
2017-10-03 23:13:32 -0600

    c11

--- IProyectoWeb JeannetteRojas-NatalinViquez/js/consultas.js
+++ IProyectoWeb JeannetteRojas-NatalinViquez/js/consultas.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 	debugger;
 	$('#bConsulta1').click(function(){
+		debugger;
 		var mayorCantidad = 0;
 		for(var i=0, l=localStorage.length; i < l; i++) {
 			debugger;
 			key = localStorage.key(i);
 			personaGuardada = localStorage.getItem(key);
             personaGuardada = JSON.parse(personaGuardada);
+            debugger;
             if(personaGuardada.productosAdquiridos > mayorCantidad){
             	debugger;
             	mayorCantidad = personaGuardada.productosAdquiridos;
-            	alert('La persona' + personaGuardada.nombre + 'es la que ha adquirido más productos:' + mayorCantidad);            	
+            	alert('La persona ' + personaGuardada.nombre + 'es la que ha adquirido más productos:' + mayorCantidad);            	
+            	break;
+            }
+            else{
+            	alert('Prueba La persona ' + personaGuardada.nombre + ' es la que ha adquirido más productos:' + mayorCantidad);            	
+
             }
 
 		}
